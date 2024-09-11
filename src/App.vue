f2c07c9f1dddd5f7bd9e119446757d47646c987b
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-11 23:22:39 +0200

    fetch events

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
 import BookingItem from '@/components/BookingItem.vue'
+
+import { ref, onMounted } from 'vue'
+
+const events = ref([])
+
+const fetchEvents = async () => {
+  const response = await fetch('http://localhost:3001/events')
+  events.value = await response.json()
+  console.log(events.value, 'events')
+}
+
+onMounted(() => {
+  fetchEvents()
+})
+
 </script>
 <template>
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i" title="Vue Conference 20204" when="2024-05-01" description="Conference about Vue and JavaScript" @register="console.log('register')" />
+      <EventCard v-for="event in events"
+                 :key="event.id"
+                 :title="event.title"
+                 :when="event.date"
+                 :description="event.description"
+                 @register="console.log('register')" />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
