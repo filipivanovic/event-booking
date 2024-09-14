dcb3e1cf501ab01923dea265cccd1de0dae2a1b8
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-14 12:52:42 +0200

    using ErrorCard to display error message in all components

--- src/components/BookingList.vue
+++ src/components/BookingList.vue
@@ -4,8 +4,9 @@ import BookingItem from '@/components/BookingItem.vue'
 import LoadingBookingItem from '@/components/LoadingBookingItem.vue'
 import { onMounted } from 'vue'
 import useBookings from '@/composable/useBookings.js'
+import ErrorCard from '@/components/ErrorCard.vue'
 
-const { bookings, loading, fetchBookings, cancelBooking } = useBookings()
+const { bookings, loading, fetchBookings, cancelBooking, error } = useBookings()
 
 onMounted(() => {
   fetchBookings()
@@ -14,17 +15,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="grid grid-cols-1 gap-4">
-    <template v-if="!loading">
-      <BookingItem v-for="booking in bookings"
-                   :key="booking" :title="booking.eventTitle"
-                   :status="booking.status"
-                   @cancelled="cancelBooking(booking.id)"></BookingItem>
-    </template>
-    <template v-else>
-      <LoadingBookingItem v-for="i in 4" :key="i"></LoadingBookingItem>
-    </template>
-  </section>
+  <template v-if="error">
+    <ErrorCard :retry="fetchBookings">
+      Failed to fetch bookings.
+    </ErrorCard>
+  </template>
+  <template v-else>
+    <section class="grid grid-cols-1 gap-4">
+      <template v-if="!loading">
+        <BookingItem v-for="booking in bookings"
+                     :key="booking" :title="booking.eventTitle"
+                     :status="booking.status"
+                     @cancelled="cancelBooking(booking.id)"></BookingItem>
+      </template>
+      <template v-else>
+        <LoadingBookingItem v-for="i in 4" :key="i"></LoadingBookingItem>
+      </template>
+    </section>
+  </template>
 </template>
 
 <style scoped>
